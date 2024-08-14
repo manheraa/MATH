edf79d465209e04b6264606c3e7e18d4305b182a
manheraa <manuheralagi4@gmail.com>
2024-08-14 20:43:05 +0530

    a

--- app.py
+++ app.py
@@ -31,25 +31,6 @@ db = FAISS.load_local("faiss_index", embeddings, allow_dangerous_deserialization
 
 retv = db.as_retriever(search_type="similarity", search_kwargs={"k": 5})
 
-#Step 5 - here we can explore how similar documents to the query are returned by prining the document metadata. This step is optional
-
-docs = retv.invoke('what is Laplace rule')
-
-def pretty_print_docs(docs):
-    print(
-        f"\n{'-' * 100}\n".join(
-            [f"Document {i+1}:\n\n" + d.page_content for i, d in enumerate(docs)]
-        )
-    )
-
-pretty_print_docs(docs)
-
-docs1 = []
-
-for doc in docs:
-    #print(doc)
-    docs1.append({"snippet": doc.page_content})
-    #print(docs1)
 
 
 #Step 4 - here we create a retrieval chain that takes llm , retirever objects and invoke it to get a response to our query
